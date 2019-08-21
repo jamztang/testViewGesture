7dff81499bca08dde33b760d199e3d3d3e235c7d
James Tang <james@goodnotesapp.com>
2019-08-21 14:48:23 +0800

    Initial Commit

---
+++ testViewGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testViewGesture
+//
+//  Created by James Tang on 21/8/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
