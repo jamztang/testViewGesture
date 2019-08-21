79811c71e4c2fcb6ee539d0ab24399386003736d
James Tang <james@goodnotesapp.com>
2019-08-21 17:06:44 +0800

    Add scrollview gesture recognizer to a UIView on top and show events

--- testViewGesture/ViewController.swift
+++ testViewGesture/ViewController.swift
@@ -11,19 +11,69 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var trackingView: TrackingView!
+    @IBOutlet weak var indicatorView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+        scrollView.contentSize = indicatorView.bounds.size
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 3.0
+//        scrollView.zoomScale = 0.1
+
         panGestureRecognizer.delegate = self
-        panGestureRecognizer.addTarget(self, action: #selector(handlePanGesture(_:)))
-    }
+        scrollView.panGestureRecognizer.addTarget(self, action: #selector(handleScrollGesture))
+        scrollView.pinchGestureRecognizer?.addTarget(self, action: #selector(handlePinchGesture))
     
+//        let hover = UIHoverGestureRecognizer(target: self, action: #selector(hovering(_:)))
+//        trackingView.addGestureRecognizer(hover)
+        trackingView.addGestureRecognizer(scrollView.panGestureRecognizer)
+        if let pinchGesture = scrollView.pinchGestureRecognizer {
+            trackingView.addGestureRecognizer(pinchGesture)
+        }
+    }
+
+    @objc
+    func hovering(_ recognizer: UIHoverGestureRecognizer) {
+        switch recognizer.state {
+        case .began, .changed:
+            Swift.print("hovering \(recognizer.location(in: trackingView))")
+            indicatorView.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        case .ended:
+            indicatorView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        default:
+            break
+        }
+    }
+
     @objc func handlePanGesture(_ recognizer: UIPanGestureRecognizer) {
         Swift.print("pan \(recognizer.location(in: trackingView))")
+    }
+    
+    @objc func handleScrollGesture(_ recognizer: UIPanGestureRecognizer) {
+        Swift.print("scroll \(recognizer.location(in: trackingView))")
+    }
+    @objc func handlePinchGesture(_ recognizer: UIPanGestureRecognizer) {
+        Swift.print("pinch \(recognizer.location(in: trackingView))")
     }
 }
 
 extension ViewController: UIGestureRecognizerDelegate {
     
+}
+
+extension ViewController: UIScrollViewDelegate {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        Swift.print("scroll \(scrollView.contentOffset)")
+//    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        Swift.print("zoom \(scrollView.zoomScale)")
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return indicatorView
+    }
 }
